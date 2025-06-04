6a75bc1b7caa738f072f042dc0c871ffaffe3e79
rgpq25 <renzopq@hotmail.com>
2025-06-04 15:19:07 -0500

    feat(web): Removed unnecesary components

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@repo/ui/button';
 import Image, { type ImageProps } from 'next/image';
 import styles from './page.module.css';
 
@@ -64,9 +63,6 @@ export default function Home() {
                         Read our docs
                     </a>
                 </div>
-                <Button appName="web" className={styles.secondary}>
-                    Open alert
-                </Button>
             </main>
             <footer className={styles.footer}>
                 <a
